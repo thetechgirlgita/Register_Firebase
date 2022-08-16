7cbc4053582c2cbe9e2dc5e93ef3ac2d18d84c9a
thetechgirlgita <thetechgirlgita@gmail.com>
2022-08-16 17:19:46 +0530

    Initial page complete

--- lib/InitialPage.dart
+++ lib/InitialPage.dart
@@ -4,12 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class InitialPage extends StatefulWidget {
-   InitialPage({Key? key}) : super(key: key);
+  InitialPage({Key? key}) : super(key: key);
 
   @override
   _InitialPageState createState() => _InitialPageState();
-
-
 }
 
 class _InitialPageState extends State<InitialPage> {
@@ -17,7 +15,7 @@ class _InitialPageState extends State<InitialPage> {
   final passwordController = TextEditingController();
   final GlobalKey<FormState> Fkey = GlobalKey<FormState>();
   @override
-  void dispose(){
+  void dispose() {
     emailController.dispose();
     passwordController.dispose();
 
@@ -30,93 +28,103 @@ class _InitialPageState extends State<InitialPage> {
         body: Align(
             alignment: Alignment.center,
             child: Container(
-                height: MediaQuery
-                    .of(context)
-                    .size
-                    .height / 2,
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width / 1.15,
+                height: MediaQuery.of(context).size.height / 1,
+                width: MediaQuery.of(context).size.width / 1.15,
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-
                       Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child:
-                   Container(
-                     height: 150,
-                     width: 200,
-
-                     child:
-                      Image.asset('logo/assets/ehobby-logo.png',
-                        fit: BoxFit.cover,
+                        padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(context).size.height * 0.15),
+                        child: Container(
+                          height: 100,
+                          width: 200,
+                          child: Image.asset(
+                            'logo/assets/ehobby-logo.png',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
-                   ),),
                       Form(
                           key: Fkey,
-                          child: Column(children: [
-                            TextFormField(
-
+                          child: Column(
+                            children: [
+                              TextFormField(
                                 decoration: const InputDecoration(
                                   hintText: 'Email',
                                 ),
-
-                              controller: emailController,
-
-                                ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextField(
-                              decoration: const InputDecoration(
-                                hintText: 'Password',
+                                controller: emailController,
                               ),
-                              controller: passwordController,
-                              obscureText: true,
-                            ),
-                            //validator: (String? password) {
-                            //                                 if (password == null || password.isEmpty) {
-                            //                                   return 'Email please';
-                            //                                 }
-                            //                                 return null;
-                            //                               },
-                            Padding(
-                              padding: EdgeInsets.only(top: 30),
-                              child:
-                            ElevatedButton(
-                                onPressed: (){
-                                  login();
-                                },
-                                // TODO : Check the user's credentials if it match then allow user to navigate to home page
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              TextField(
+                                decoration: const InputDecoration(
+                                  hintText: 'Password',
+                                ),
+                                controller: passwordController,
+                                obscureText: true,
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(top: 50),
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.pinkAccent,
+
+                                    ),
+
+                                      onPressed: () {
+                                        //login();
+                                      },
+                                      // TODO : Check the user's credentials if it match then allow user to navigate to home page
+                                      child: Container(
+
+                                        alignment: Alignment.center,
+                                        height: 40,
+                                        width: 150,
+                                        child: const Text(
+                                          "Login",
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            //fontFamily: 'Roboto',
+                                          ),
+                                          //textAlign: TextAlign.center,
+                                        ),
+                                      ))),
+                              Padding(padding: EdgeInsets.only(top: 180),
                                 child: Container(
                                   alignment: Alignment.center,
-                                  height: 40,
-                                  width: 150,
-                                  child: const Text("Login",
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children:  [
+                                      const  Text("Don't have an account? ",
                                     style: TextStyle(
                                       fontSize: 15,
-                                      //fontFamily: 'Roboto',
-                                    ),
-                                    //textAlign: TextAlign.center,
+                                    ), //textAlign: TextAlign.center,
+                                  ),
+                                      GestureDetector(
+                                        child:  const Text('Register',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.pinkAccent,
+                                          ),
+                                        ),
+                                        onTap: (){
+                                         // return RegisterPage();
+                                        },
+
+                                      )
+                                    ]
                                   )
-                                  ,
-                                )))
-                          ],
+                                )
+                              ),
 
+                            ],
                           )),
-
-
-                    ]))));}
-    Future login () async {
-     await FirebaseAuth.instance.signInWithEmailAndPassword(
-         email: emailController.text.trim(),
-         password: passwordController.text.trim(),
-     );
-
+                    ]))));
   }
 
-}
 
+}
